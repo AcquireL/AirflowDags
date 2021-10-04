c6dc8d86cbd457d62511734d8bd57b67ce3369a5
v_laiwanjun <v_laiwanjun@baidu.com>
2021-10-04 12:04:07 +0800

    first commit

---
+++ dags/bash_operator_test.py
@@ -0,0 +1,62 @@
+import datetime
+from builtins import range
+from datetime import timedelta
+
+import pytz as pytz
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+tz = pytz.timezone('Asia/Shanghai')
+dt = datetime.now(tz=tz) - timedelta(days=1)
+utc_dt = dt.astimezone(pytz.utc).replace(tzinfo=None)
+
+
+# 定义默认参数
+default_args = {
+    'owner': 'lwj',  # 拥有者名称
+    'start_date': utc_dt,  # 第一次开始执行的时间，为格林威治时间，为了方便测试，一般设置为当前时间减去执行周期
+    'retries': 1,  # 失败重试次数
+    'retry_delay': timedelta(minutes=20)  # 失败重试间隔
+}
+
+# 定义DAG
+dag = DAG(
+    dag_id='bash_operator_test',
+    default_args=default_args,
+    schedule_interval="5/* * * * *",
+    catchup=False
+)
+
+start_run = DummyOperator(
+    task_id='start_run',
+    dag=dag,
+)
+
+# [START howto_operator_bash]
+run_echo = BashOperator(
+    task_id='run_echo',
+    bash_command='echo 1',
+    dag=dag,
+)
+# [END howto_operator_bash]
+
+start_run >> run_echo
+
+# [START howto_operator_python]
+def print_context(ds, **kwargs):
+    print(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+
+run_python_print = PythonOperator(
+    task_id='run_python_print',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag,
+)
+# [END howto_operator_python]
+
+run_echo >> run_python_print
